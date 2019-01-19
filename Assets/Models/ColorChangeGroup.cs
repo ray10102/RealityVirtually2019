8331c72d62f776549ca071d7acf024ab3f190fcd
guozhang34 <guozhang34@gmail.com>
2019-01-19 13:03:55 -0500

    Commit

---
+++ Assets/Models/ColorChangeGroup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeGroup : MonoBehaviour
+{
+    private ColorChanger[] children;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        children = GetComponentsInChildren<ColorChanger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad % 2 < .2 )
+        {
+            for (int i = 0; i < children.Length; i++)
+            {
+                children[i].ChangeColor(new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+            }
+        }
+    }
+}
---
+++ Assets/Models/ColorChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    private MeshRenderer[] renderers;
+    [SerializeField]
+    private bool shouldLerp;
+    [SerializeField]
+    private Color lerpTo;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private int rendererCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderers = GetComponentsInChildren<MeshRenderer>();
+        shouldLerp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldLerp)
+        {
+            for (int i = 0; i < rendererCount; i++)
+            {
+                renderers[i].material.color = Color.Lerp(renderers[i].material.color, lerpTo, speed * Time.deltaTime);
+            }
+        }
+    }
+   
+    public void ChangeColor(Color color)
+    {
+        shouldLerp = true;
+        lerpTo = color;
+    }
+ 
+}
