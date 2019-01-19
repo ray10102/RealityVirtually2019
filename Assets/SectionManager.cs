2cfe53b52d88fd906b1f8c8ed7704ff48b51b985
ray <ray10102@gmail.com>
2019-01-19 18:41:46 -0500

    added models

---
+++ Assets/SectionManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.Collections;
+using UnityEngine;
+
+public class SectionManager : MonoBehaviour {
+	
+	[SerializeField]
+	private ObjectCollection[] collections;
+
+	private float percentage;
+
+	[SerializeField]
+	private int rows;
+
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		collections = GetComponentsInChildren<ObjectCollection>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void UpdateChildren()
+	{
+		
+	}
+}
---
+++ Assets/Timeline.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timeline : MonoBehaviour
+{
+	public UnityEvent[] actions;
+
+	public float[] timeline;
+
+	private float startTime;
+
+	private int index;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		index = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while (timeline.Length > 0 && timeline[index] < Time.timeSinceLevelLoad - startTime)
+		{
+			actions[index].Invoke();
+			index++;
+		}
+	}
+
+	public void startTimer()
+	{
+		startTime = Time.timeSinceLevelLoad;
+	}
+}
