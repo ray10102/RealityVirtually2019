aa02691a1d2f519aa18c320fabc4c127b723437a
ray <ray10102@gmail.com>
2019-01-20 13:25:41 -0500

    fixed dream strings

--- Assets/InteractiveHuman.cs
+++ Assets/InteractiveHuman.cs
@@ -12,8 +12,25 @@ public class InteractiveHuman : MonoBehaviour
 	[SerializeField]
 	private Text displayText;
 
-	[SerializeField]
-	private string[] dreams;
+	private static readonly string[] dreams = {
+		"I wanted to be a talk show host, like Oprah! --- Najla", 
+		"I wanted to be a teacher so I could write on the chalk board anytime I wanted. --- Denise Ray", 
+		"I wanted to have my own landscaping business, Get a house in Africa , and have a pet lion --- Tariq",
+		"Ballerina, run a school for girls in North Africa, own a coffee shop --- Alicia-Jewell",
+		"Wanted to be an engineer to build my own Voltron robots –-- Mohammed ",
+		"I really wanted to be a actress on television! --- Jillian ",
+		"Meteorologist, teacher and a movie star. ---Iman ",
+		"I wanted to be a firefighter, a hero in a dope uniform and save people from burning buildings. --- Bilal",
+		"Pineapple farmer. Boat tour guide. Outdoor survivalist. --- Wesley",
+		"Astronaut, fire fighter, ballerina --- Latoya",
+		"I wanted to coach football or be a WWE superstar. --- Halim",
+		"Wanted to work on computers, own a bookstore and be a unicorn --- Sutanah",
+		"I always heard the message, seek knowledge even if you have to travel to China. So I would visualize digging my way through the earth's core to China. --- Hannah",
+		"Wanted to be like Mary Poppins, a Nanny --- Ruby",
+		"I dreamed of traveling the world. I wanted to sky dive. I wanted to go to an orangutan or other large ape conservatory in the jungle and volunteer. --- Diana",
+		"An architect, a food scientist and a marine biologist --- Jade",
+		"Explorer, marine biologist, cardiologist --- Nashid"
+	};
 
 	[SerializeField]
 	private float lerpSpeed = .5f;
@@ -23,7 +40,7 @@ public class InteractiveHuman : MonoBehaviour
 	[SerializeField]
 	private Light spotlight;
 
-	private int index = 0;
+	private static int index = 0;
 	
 	// Use this for initialization
 	void Start ()
@@ -51,7 +68,8 @@ public class InteractiveHuman : MonoBehaviour
 			SetText();
 			FadeText(1f);
 			//HighlightModel(true);
-		}	}
+		}	
+	}
 
 	private void OnTriggerExit(Collider other)
 	{
